d3a4144d864965dc5786fc7bad74442ac9cb6309
BTZ <BTZ@BTZ-LAPTOP-12>
2023-09-27 11:05:40 +0200

    -Paddel erstellt -Pong erstellt -Spieler Paddel Bewegung

--- Pong/Form1.Designer.cs
+++ Pong/Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pbPaddelSpieler = new PictureBox();
+            pbPaddelGegner = new PictureBox();
+            pbPong = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pbPaddelSpieler).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbPaddelGegner).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbPong).BeginInit();
+            SuspendLayout();
+            // 
+            // pbPaddelSpieler
+            // 
+            pbPaddelSpieler.BackColor = SystemColors.ActiveCaptionText;
+            pbPaddelSpieler.Location = new Point(12, 160);
+            pbPaddelSpieler.Name = "pbPaddelSpieler";
+            pbPaddelSpieler.Size = new Size(10, 125);
+            pbPaddelSpieler.TabIndex = 0;
+            pbPaddelSpieler.TabStop = false;
+            // 
+            // pbPaddelGegner
+            // 
+            pbPaddelGegner.BackColor = SystemColors.ActiveCaptionText;
+            pbPaddelGegner.Location = new Point(780, 160);
+            pbPaddelGegner.Name = "pbPaddelGegner";
+            pbPaddelGegner.Size = new Size(10, 125);
+            pbPaddelGegner.TabIndex = 1;
+            pbPaddelGegner.TabStop = false;
+            // 
+            // pbPong
+            // 
+            pbPong.Image = (Image)resources.GetObject("pbPong.Image");
+            pbPong.Location = new Point(383, 199);
+            pbPong.Name = "pbPong";
+            pbPong.Size = new Size(30, 30);
+            pbPong.TabIndex = 2;
+            pbPong.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(802, 453);
+            Controls.Add(pbPong);
+            Controls.Add(pbPaddelGegner);
+            Controls.Add(pbPaddelSpieler);
+            Name = "Form1";
+            Text = "Pong";
+            ((System.ComponentModel.ISupportInitialize)pbPaddelSpieler).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbPaddelGegner).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbPong).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pbPaddelSpieler;
+        private PictureBox pbPaddelGegner;
+        private PictureBox pbPong;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Pong/Form1.cs
+++ Pong/Form1.cs
@@ -6,5 +6,33 @@ namespace Pong
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MovePaddel();
+            MovePong();
+        }
+        void MovePong()
+        {
+
+        }
+        void MovePaddel() // Bewegung des Spieler Paddels
+        {
+
+            int i = Location.Y;
+            int paddelposition = Cursor.Position.Y - 100 - i;
+            if (paddelposition <= 0)
+            {
+                pbPaddelSpieler.Top = 0;
+            }
+            else if (paddelposition >= 320)
+            {
+                pbPaddelSpieler.Top = 320;
+            }
+            else
+            {
+                pbPaddelSpieler.Top = paddelposition;
+            }
+        }
     }
 }
